7db97231068e60aa9383a4b14ed66d60b921f764
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-06-24 15:30:40 +0530

    Add files via upload

---
+++ subtree.java
@@ -0,0 +1,47 @@
+package BinaryTree;
+import BinaryTree.Levelorder.Node;
+public class subtree {
+
+    public static boolean isIdentical(Node node, Node subroot) {
+        if (node == null && subroot == null) {
+            return true;
+        } else if (node == null || subroot == null || node.data != subroot.data) {
+            return false;
+        }
+        if (!isIdentical(node.left, subroot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subroot.right)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSub(Node root, Node subroot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == subroot.data) {
+            if (isIdentical(root, subroot)) {
+                return true;
+            }
+        }
+        return isSub(root.left, subroot) || isSub(root.right, subroot);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.right = new Node(3);
+        root.left = new Node(2);
+        root.left.right = new Node(5);
+        root.left.left = new Node(4);
+        root.right.right = new Node(7);
+        root.right.left = new Node(6);
+
+        Node subroot = new Node(2);
+        subroot.right = new Node(5);
+        subroot.left = new Node(4);
+
+        System.out.println(isSub(root, subroot));
+    }
+}
